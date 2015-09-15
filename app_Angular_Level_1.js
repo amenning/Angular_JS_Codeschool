94e6413595f78f74f14d8e96c8c34d25774c7bcb
Andrew Menning <menning23@gmail.com>
2015-09-14 23:15:02 -0400

    Level 1

---
+++ app_Angular_Level_1.js
@@ -0,0 +1,24 @@
+(function(){
+	var app = angular.module('store', []);
+	
+	app.controller('StoreController', function(){
+		this.products = gem;
+	});
+	
+	var gem = [
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '. . .',
+		canPurchase: true,
+		soldOut: true,
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: '. . .',
+		canPurchase: false,
+		soldOut: true,
+	}];
+	
+})();
